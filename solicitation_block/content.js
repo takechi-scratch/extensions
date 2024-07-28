d7352a0682739bcbc947decdc293da9721e55207
takechi-scratch <takechi.scratch@gmail.com>
2024-07-28 19:27:31 +0900

    勧誘ブロック公開

---
+++ solicitation_block/content.js
@@ -0,0 +1,49 @@
+// 侍エンジニア
+if (window.location.host.includes("sejuku.net")) {
+    let popup;
+    let elements;
+    const removeClasses = ["p-blogParts post_content", "c-widget widget_setting_banner_widget", "footer-banner creative-banner active", "sc_button2"];
+
+    console.log("監視を開始します");
+    setInterval(() => {
+        popup = document.getElementById("break_banner");
+        if (popup) {
+            popup.remove();
+            console.log("ポップアップを削除しました");
+        }
+
+        removeClasses.forEach((removeClass) => {
+            elements = document.getElementsByClassName(removeClass);
+            [...elements].forEach(element => {
+                element.remove();
+                console.log("勧誘の要素を削除しました");
+            });
+        });
+
+    }, 1000);
+}
+
+// テックアカデミー
+if (window.location.host.includes("magazine.techacademy.jp")) {
+    let popup;
+    let elements;
+    const removeClasses = ["ad", "service-description", "recruit-box", "post-footer-ad"];
+
+    console.log("監視を開始します");
+    setInterval(() => {
+        popup = document.getElementById("popmake-76238");
+        if (popup) {
+            popup.remove();
+            console.log("ポップアップを削除しました");
+        }
+
+        removeClasses.forEach((removeClass) => {
+            elements = document.getElementsByClassName(removeClass);
+            [...elements].forEach(element => {
+                element.remove();
+                console.log("勧誘の要素を削除しました");
+            });
+        });
+
+    }, 1000);
+}
