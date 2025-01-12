ca6f56298106c1c108d88ae5ee6aba81aca9eb32
takechi-scratch <takechi.scratch@gmail.com>
2025-01-12 20:47:09 +0900

    fix クラスとIDに分離

--- solicitation_block/content.js
+++ solicitation_block/content.js
@@ -2,15 +2,18 @@
 if (window.location.host.includes("sejuku.net")) {
     let popup;
     let elements;
-    const removeClasses = ["p-blogParts post_content", "c-widget widget_setting_banner_widget", "footer-banner creative-banner active", "sc_button2", "fv-banner_random"];
+    const removeClasses = ["p-blogParts post_content", "c-widget widget_setting_banner_widget", "footer-banner creative-banner active", "sc_button2"];
+    const removeIds = ["break_banner", "fv-banner_random"];
 
     console.log("監視を開始します");
     setInterval(() => {
-        popup = document.getElementById("break_banner");
-        if (popup) {
-            popup.remove();
-            console.log("ポップアップを削除しました");
-        }
+        removeIds.forEach((removeId) => {
+            popup = document.getElementById(removeId);
+            if (popup) {
+                popup.remove();
+                console.log("ポップアップを削除しました");
+            }
+        });
 
         removeClasses.forEach((removeClass) => {
             elements = document.getElementsByClassName(removeClass);
